80f629baefe97f0984020bf88b6f9150c7974c1f
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-03-25 20:56:54 +0200

    updated scrape.php

--- CurrencyConverter/scrape.php
+++ CurrencyConverter/scrape.php
@@ -1,19 +1,28 @@
 <?php
 
 include ("db_info.php");
+
 $scrape = curl_init();
 curl_setopt($scrape, CURLOPT_URL, "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP");
 curl_setopt($scrape, CURLOPT_RETURNTRANSFER, true);
+
 $result = curl_exec($scrape);
 $data = json_encode($result);
 $string_data = explode(",", $data);
 $rate_string = explode("]", $string_data[sizeof($string_data)-1]);
 $current_rate = $rate_string[0];
-echo $current_rate;
+
 $mysql = $mysqli->prepare("INSERT INTO rates(rate) VALUES (?)");
 $mysql->bind_param('i', $current_rate);
 $mysql->execute();
-$mysql2 = $mysqli->prepare("SELECT rate FROM rates WHERE ID= (SELECT COUNT(rate) FROM rates) as last_rate");
+
+$mysql2 = $mysqli->prepare("SELECT rate FROM rates WHERE ID = (SELECT COUNT(rate) FROM rates)");
 $mysql2->execute();
+
+$rate = $mysql2->get_result();
+$row = $rate->fetch_assoc();
+echo $row["rate"];
+
 curl_close($scrape);
+
 ?>
