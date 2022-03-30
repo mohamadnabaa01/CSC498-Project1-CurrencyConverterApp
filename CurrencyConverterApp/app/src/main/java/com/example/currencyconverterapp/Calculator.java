0a6d04a49ce6a09c7c8765dfb6c95e6c40278d91
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-03-30 18:25:11 +0300

    added some texts and list view to the calculator page

--- CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/Calculator.java
+++ CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/Calculator.java
@@ -1,6 +1,7 @@
 package com.example.currencyconverterapp;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +14,7 @@ public class Calculator extends AppCompatActivity{
         setContentView(R.layout.calculator);
         Intent y = getIntent();
         String dollar_current_rate = y.getStringExtra("dollar_rate");
+        TextView dollar_text = (TextView) findViewById(R.id.current_rate_text);
+        dollar_text.setText(dollar_current_rate + " L.L.");
     }
 }
