0a9af2a2f137a8ef4a55bc1fef1aff3ee7556a07
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-04-01 22:48:38 +0300

    removed volley

--- CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/Calculator.java
+++ CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/Calculator.java
@@ -11,13 +11,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
