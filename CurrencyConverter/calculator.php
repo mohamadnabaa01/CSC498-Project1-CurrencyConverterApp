12dc7af2f8f285c071ed7ae47fcfc3bb7429bdab
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-04-02 15:47:28 +0300

    calculating result

--- CurrencyConverter/calculator.php
+++ CurrencyConverter/calculator.php
@@ -11,11 +11,18 @@ $response = [];
 $response["amount_convert"]=$amount;
 $response["currency_type"]=$currency_type;
 $response["current_rate"]=$current_rate;
-$data_result = json_encode($response);
-echo $data_result;
+
 
 $query=$mysqli->prepare("INSERT INTO convert_currencies(amount,amount_type,current_rate) VALUES (?,?,?)");
 $query->bind_param("isi",$amount,$currency_type,$current_rate);
 $query->execute();
+if($currency_type=="USD"){
+$result=$amount*$current_rate;
+}else if($currency_type="L.L."){
+$result=$amount/$current_rate;
+}
+$response["result"]=$result;
+$data_result = json_encode($response);
+echo $data_result;
 
 ?>
